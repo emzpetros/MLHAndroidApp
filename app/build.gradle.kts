59b4dd2c616556b2b6a2c9db8b4e6824ad272b3d
Eileen Petros <emp110@case.edu>
2024-01-09 14:32:20 -0500

    app buildable and runnable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.cheerify"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cheerify"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -39,6 +39,8 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.android.volley:volley:1.2.1")
+//    implementation()
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/cheerify/MainActivity.kt
+++ app/src/main/java/com/example/cheerify/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.cheerify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var affirmation: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val refresh = findViewById<Button>(R.id.refresh_button)
+        affirmation = String()
+        getAffirmation()
+        refreshAffirmation(refresh)
+    }
+
+    private fun getAffirmation(){
+
+    }
+
+    private fun refreshAffirmation(button: Button){
+        button.setOnClickListener{
+            getAffirmation()
+        }
     }
 }
