63aa15dfec6759a3a5eb0862b567a8a404c8275f
Eileen Petros <emp110@case.edu>
2024-01-10 13:01:46 -0500

    part 2 done, dependcies fixed, room database implementation started

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,7 +35,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
@@ -42,4 +43,44 @@ dependencies {
     implementation("com.codepath.libraries:asynchttpclient:2.2.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    implementation ("androidx.appcompat:appcompat:$rootProject.appCompatVersion")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
+// Dependencies for working with Architecture components
+    // You'll probably have to update the version numbers in build.gradle (Project)
+
+    // Room components
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt( "androidx.room:room-compiler:2.6.1")
+    androidTestImplementation ("androidx.room:room-testing:2.6.1")
+
+    // Lifecycle components
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.6.2")
+
+    // Kotlin components
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.31")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
+    api ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0-RC2")
+
+    // UI
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.android.material:material:1.11.0")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
+//    androidTestImplementation ("androidx.test.espresso:espresso-core:$rootProject.espressoVersion", {
+//        exclude group: 'com.android.support', module: 'support-annotations'
+//    })
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
+
+
+
+
+
+
